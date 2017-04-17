238cb0b5b7249dc8053e6bbb4f3a0f434b2eb990
Ilya Ismagilov <ismagilov@gmail.com>
2017-04-17 09:47:39 +0700

    Solved

---
+++ src/algorithms/strings/Pangrams.java
@@ -0,0 +1,32 @@
+/**
+* https://www.hackerrank.com/challenges/pangrams
+*/
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        int[] letters = new int[26];
+        
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (Character.isLetter(c))
+                letters[(byte)Character.toLowerCase(c) - (byte)'a']++;
+        }
+            
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i] == 0) {
+                System.out.println("not pangram");            
+                return;
+            }
+        }        
+        
+        System.out.println("pangram");
+    }
+}
