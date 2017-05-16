41afe9ebdcd5d6b46e0f784d9305c88bab45e4a8
Ilya Ismagilov <ismagilov@gmail.com>
2017-05-16 16:10:13 +0700

    Solved part of insertion sorting

---
+++ src/algorithms/sorting/InsertionSort1.java
@@ -0,0 +1,42 @@
+package algorithms.sorting;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void insertIntoSorted(int[] ar) {
+        int val = ar[ar.length - 1];
+        
+        int i = ar.length - 1;
+        while (i > 0 && ar[i - 1] > val) {
+            ar[i] = ar[i - 1];
+            i--;
+            printArray(ar);
+        }
+        
+        ar[i] = val;
+        printArray(ar);
+    }
+    
+    
+/* Tail starts here */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        int[] ar = new int[s];
+        for(int i=0;i<s;i++){
+            ar[i]=in.nextInt(); 
+        }
+        insertIntoSorted(ar);
+    }
+    
+    private static void printArray(int[] ar) {
+        for(int n: ar){
+            System.out.print(n+" ");
+        }
+        System.out.println("");
+    }  
+}
