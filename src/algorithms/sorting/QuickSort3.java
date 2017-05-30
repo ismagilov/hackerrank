5786ee208f893da252adbd745593ff6cbe710548
Ilya Ismagilov <ismagilov@gmail.com>
2017-05-30 17:50:42 +0700

    Fix array initialization from System.in.

--- src/algorithms/sorting/QuickSort3.java
+++ src/algorithms/sorting/QuickSort3.java
@@ -11,9 +11,7 @@ public class QuickSort3 {
         Scanner in = new Scanner(System.in);
 
         int n = in.nextInt();
-
-        int[] arr = {1, 3, 9, 8, 2, 7, 5};
-
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++)
             arr[i] = in.nextInt();
 
